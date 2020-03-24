cdc7081dc4fdfcafa78b61a2bd123b6583323576
BentGunnarsson <bent19@ru.is>
2020-03-24 15:03:15 +0000

    no negatives allowed

--- Kata.py
+++ Kata.py
@@ -1,11 +1,25 @@
 def Add(entry):
     if entry == "":
         return 0
-    if "," not in entry and "\n" not in entry:
-        return int(entry)
+    
     entry = entry.replace("\n",",")
     entry = entry.split(",")
     ret_val = 0
+    negatives = False
+
     for num in entry:
-        ret_val += int(num)
+        if int(num) < 0:
+            negatives = True
+            break
+        if int(num) < 1001:
+            ret_val += int(num)
+
+    if negatives:
+        neg_nums = ""
+        for num in entry:
+            if int(num) < 0:
+                neg_nums += ","+str(num)
+        neg_nums = neg_nums[1:]
+        raise Exception("Negatives not allowed: "+neg_nums)
+
     return ret_val
--- test_Kata.py
+++ test_Kata.py
@@ -1,4 +1,5 @@
 from Kata import Add
+import pytest
 
 def test_add_nothing():
     assert Add("") == 0
@@ -14,3 +15,10 @@ def test_add_unknown_amt_of_numbers():
 
 def test_add_with_newline_delimiter():
     assert Add("1\n2,3") == 6
+
+def test_ignore_above_1000():
+    assert Add("1001,4") == 4
+
+def test_negatives_exception():
+    with pytest.raises(Exception):
+        assert Add("-2,4,-5,-7") == "No negatives allowed: -2,-5,-7"
