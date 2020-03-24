ed9910f8f565d206a3b3cd3a05d879834e6e11e0
BentGunnarsson <bent19@ru.is>
2020-03-24 15:15:49 +0000

    fix

--- Kata.py
+++ Kata.py
@@ -1,13 +1,17 @@
 def Add(entry):
     if entry == "":
         return 0
+
     if entry[:2] == "//":
+        # Adding a new delimiter
         delim = entry[2]
         entry = entry[4:]
         entry = entry.replace(delim,",")
 
     entry = entry.replace("\n",",")
+
     entry = entry.split(",")
+
     ret_val = 0
     negatives = False
 
@@ -19,6 +23,7 @@ def Add(entry):
             ret_val += int(num)
 
     if negatives:
+        # Finding all the negative numbers
         neg_nums = ""
         for num in entry:
             if int(num) < 0:
@@ -27,6 +32,3 @@ def Add(entry):
         raise Exception("Negatives not allowed: "+neg_nums)
 
     return ret_val
-
-if __name__ == "__main__":
-    Add("//%\n1%2%3")
